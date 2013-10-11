d035a199b30cb8b926713974c453d13a5e94994d
tthieman <travis.thieman@gmail.com>
2013-10-11 15:19:22 -0400

    Remove flymake mode

--- init.el
+++ init.el
@@ -121,23 +121,6 @@
 
 (ido-mode)
 
-; epylint for flymake
-(setq epylint "/usr/local/bin/epylint")
-(when (load "flymake" t)
-  (defun flymake-pylint-init ()
-	(let* ((temp-file (flymake-init-create-temp-buffer-copy
-					   'flymake-create-temp-inplace))
-		   (local-file (file-relative-name
-						temp-file
-						(file-name-directory buffer-file-name))))
-	  (list epylint (list local-file))))
-
-  (add-to-list 'flymake-allowed-file-name-masks
-			   '("\\.py\\'" flymake-pylint-init)))
-
-; uncomment to enable flymake-mode on python file load
-; (add-hook 'find-file-hook 'flymake-find-file-hook)
-
 ; fix for env in gui emacs
 (defun setenv-from-shell (varname)
   (setenv varname (replace-regexp-in-string
