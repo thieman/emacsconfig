ed7904bab4977a9d6449da36a489e604fb6d8bd1
Travis Thieman <travis.thieman@gmail.com>
2014-01-28 22:48:01 -0500

    Setup flx-ido

--- init.el
+++ init.el
@@ -154,8 +154,6 @@
 
 (add-to-list 'load-path "~/.emacs.d/vendor/bash-completion")
 
-(ido-mode)
-
 ;; jedi
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t)
@@ -227,9 +225,12 @@
             (lambda ()
               (exec-path-from-shell-initialize)
               (exec-path-from-shell-copy-env "PYTHONPATH"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;; flx-ido
+(add-hook 'after-init-hook
+       (lambda ()
+         (require 'flx-ido)
+         (ido-mode t)
+         (ido-everywhere t)
+         (flx-ido-mode t)
+         (setq ido-use-faces nil)))
