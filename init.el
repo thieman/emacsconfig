31ab83c485c44a5e8df7515db7732b11c1c35a2a
tthieman <travis.thieman@gmail.com>
2014-02-25 10:59:09 -0500

    Remove pymacs and ropemacs

--- init.el
+++ init.el
@@ -159,37 +159,6 @@
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
 
-;; rope and pymacs
-(after 'auto-complete
-       (add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
-       (setq ac-use-menu-map t)
-       (define-key ac-menu-map "\C-n" 'ac-next)
-       (define-key ac-menu-map "\C-p" 'ac-previous))
-
-(after 'auto-complete-config
-       (ac-config-default)
-       (when (file-exists-p (expand-file-name "~/.emacs.d/elisp/Pymacs"))
-         (ac-ropemacs-initialize)
-         (ac-ropemacs-setup)))
-
-(after 'auto-complete-autoloads
-       (autoload 'auto-complete-mode "auto-complete" "enable auto-complete-mode" t nil)
-       (add-hook 'python-mode-hook
-                 (lambda ()
-                   (require 'auto-complete-config)
-                   (add-to-list 'ac-sources 'ac-source-ropemacs)
-                   (auto-complete-mode))))
-
-;; pymacs
-(add-to-list 'load-path "~/.emacs.d/elisp")
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-(require 'pymacs)
-(pymacs-load "ropemacs" "rope-")
-
 ;; ag
 (setq ag-highlight-search 't)
 (setq ag-reuse-buffers 't)
