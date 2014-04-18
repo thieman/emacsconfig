12fa215dd1daa032c104ffa9810db1e3d2d414bf
Travis Thieman <travis.thieman@gmail.com>
2014-04-18 09:35:38 -0400

    Add JSON pretty printing on region

--- init.el
+++ init.el
@@ -222,3 +222,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(defun json-format ()
+  (interactive)
+  (save-excursion
+    (shell-command-on-region (mark) (point) "python -m json.tool" (buffer-name) t)))
