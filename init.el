f77c75fe2d90444addc73df59665557473894832
Travis Thieman <travis.thieman@gmail.com>
2015-04-30 13:36:28 -0400

    ADD A BUNCHA GO STUFF

--- init.el
+++ init.el
@@ -16,14 +16,20 @@
     exec-path-from-shell
     flx-ido
     gh
+    go-mode
+    go-play
+    go-projectile
     handlebars-mode
     helm
     helm-projectile
     jedi
     jinja2-mode
     js2-mode
+    js-doc
     less-css-mode
+    lua-mode
     magit
+    markdown-mode
     minimap
     powerline
     projectile
@@ -31,6 +37,8 @@
     smex
     soundcloud
     sublime-themes
+    undo-tree
+    wsd-mode
     yaml-mode))
 
 (defun thieman-install-packages ()
@@ -55,6 +63,9 @@
   `(when (or (string-match "darwin" (emacs-version))
              (not (string-match "arm" (emacs-version))))
      (progn ,@body)))
+
+; confirm on kill
+(setq confirm-kill-emacs 'yes-or-no-p)
 
 ; theme
 (add-hook 'after-init-hook (lambda () (load-theme 'hickey t)))
@@ -100,7 +111,7 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq sgml-basic-offset 4)  ; use 4 space indent in html mode
 (setq handlebars-basic-offset 4)
-(setq js-indent-level 2)
+(setq js-indent-level 4)
 (add-to-list 'auto-mode-alist '("\\.html\\'" . jinja2-mode))  ;; use jinja2-mode for html files
 
 ; key bindings
@@ -110,6 +121,7 @@
 (global-set-key (kbd "M-s") 'soundcloud)
 (global-set-key (kbd "C-x g") 'ag-project)
 (global-set-key (kbd "M-p") 'github-pulls)
+(global-set-key (kbd "C-c C-p") 'compile)
 
 (setq initial-scratch-message "")
 
@@ -165,8 +177,10 @@
  '(custom-enabled-themes (quote (hickey)))
  '(custom-safe-themes
    (quote
-    ("ce79400f46bd76bebeba655465f9eadf60c477bd671cbcd091fe871d58002a88" "9bcb8ee9ea34ec21272bb6a2044016902ad18646bd09fdd65abae1264d258d89" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bc89fda3d232a3daa4eb3a9f87d6ffe1272fea46e4cf86686d9e8078e4209e2c" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" "dc46381844ec8fcf9607a319aa6b442244d8c7a734a2625dac6a1f63e34bc4a6" "978bd4603630ecb1f01793af60beb52cb44734fc14b95c62e7b1a05f89b6c811" "29a4267a4ae1e8b06934fec2ee49472daebd45e1ee6a10d8ff747853f9a3e622" "61d1a82d5eaafffbdd3cab1ac843da873304d1f05f66ab5a981f833a3aec3fc0" "d293542c9d4be8a9e9ec8afd6938c7304ac3d0d39110344908706614ed5861c9" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "e26780280b5248eb9b2d02a237d9941956fc94972443b0f7aeec12b5c15db9f3" "f220c05492910a305f5d26414ad82bf25a321c35aa05b1565be12f253579dec6" "d0ff5ea54497471567ed15eb7279c37aef3465713fb97a50d46d95fe11ab4739" default)))
+    ("298f3826066ad761b9f461eec62b578c9725fd6134fdcab75fef579032f03d34" "ce79400f46bd76bebeba655465f9eadf60c477bd671cbcd091fe871d58002a88" "9bcb8ee9ea34ec21272bb6a2044016902ad18646bd09fdd65abae1264d258d89" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bc89fda3d232a3daa4eb3a9f87d6ffe1272fea46e4cf86686d9e8078e4209e2c" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" "dc46381844ec8fcf9607a319aa6b442244d8c7a734a2625dac6a1f63e34bc4a6" "978bd4603630ecb1f01793af60beb52cb44734fc14b95c62e7b1a05f89b6c811" "29a4267a4ae1e8b06934fec2ee49472daebd45e1ee6a10d8ff747853f9a3e622" "61d1a82d5eaafffbdd3cab1ac843da873304d1f05f66ab5a981f833a3aec3fc0" "d293542c9d4be8a9e9ec8afd6938c7304ac3d0d39110344908706614ed5861c9" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "e26780280b5248eb9b2d02a237d9941956fc94972443b0f7aeec12b5c15db9f3" "f220c05492910a305f5d26414ad82bf25a321c35aa05b1565be12f253579dec6" "d0ff5ea54497471567ed15eb7279c37aef3465713fb97a50d46d95fe11ab4739" default)))
  '(inhibit-startup-screen t)
+ '(js2-basic-offset 4)
+ '(js2-strict-inconsistent-return-warning nil)
  '(projectile-test-prefix-function (quote default-projectile-prefix-unless-django))
  '(projectile-test-suffix-function (quote default-projectile-suffix-unless-django))
  '(scroll-bar-mode nil)
@@ -219,6 +233,10 @@
                ((member project-type '(rails-test ruby-test lein django python)) "_test")
                ((member project-type '(maven symfony)) "Test")))))
 
+;; flyspell-prog-mode
+(add-hook 'python-mode-hook 'flyspell-prog-mode)
+(add-hook 'clojure-mode-hook 'flyspell-prog-mode)
+
 (defun default-projectile-prefix-unless-django (project-type)
   (when (not (eq project-type 'django))
     (projectile-test-prefix project-type)))
@@ -228,13 +246,12 @@
       "_test"
     (projectile-test-suffix project-type)))
 
-
-
 ;; exec-path-from-shell
 (when (memq window-system '(mac ns))
   (add-hook 'after-init-hook
             (lambda ()
               (exec-path-from-shell-initialize)
+              (exec-path-from-shell-copy-env "GOPATH")
               (exec-path-from-shell-copy-env "PYTHONPATH"))))
 
 ;; flx-ido
@@ -250,7 +267,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#1D2021" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Source Code Pro for Powerline")))))
+
+;; js2-mode
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; js-doc
+(setq js-doc-mail-address "travis.thieman@gmail.com"
+      js-doc-author (format "Travis Thieman <%s>" js-doc-mail-address)
+      js-doc-url "http://github.com/thieman")
+(add-hook 'js2-mode-hook
+          #'(lambda ()
+              (define-key js2-mode-map "\C-ci" 'js-doc-insert-function-doc)
+              (define-key js2-mode-map "@" 'js-doc-insert-tag)))
 
 (defun json-format ()
   (interactive)
@@ -262,3 +291,32 @@
   (when buffer-file-name (save-buffer)))
 (defadvice other-window (before other-window-now activate)
   (when buffer-file-name (save-buffer)))
+
+;; cider stuff
+(setq cider-show-error-buffer nil)
+
+;; undo-tree
+(after 'undo-tree
+       (require 'undo-tree)
+       (global-undo-tree-mode t))
+
+;; go-mode
+(defun my-go-mode-hook ()
+  ; Use goimports instead of gofmt
+  (setq gofmt-command "goimports")
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "C-c .") 'godef-jump)
+  ; Praise Go Oracle
+  (go-oracle-mode))
+
+(after 'go-mode
+       (require 'go-projectile)
+       (load-file "$GOPATH/src/code.google.com/p/go.tools/cmd/oracle/oracle.el"))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
