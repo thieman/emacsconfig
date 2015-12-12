c3f709e221cdcedc5ebf90e87622cb50f9693801
Travis Thieman <travis.thieman@gmail.com>
2015-12-11 20:57:04 -0500

    Add back magit-gh-pulls while we improve it

--- init.el
+++ init.el
@@ -327,3 +327,8 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
+
+(add-hook 'after-init-hook
+          (lambda ()
+            (require 'magit-gh-pulls)
+            (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)))
